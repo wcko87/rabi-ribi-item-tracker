6caa101fb2bc8487544cf09221c900ba6490d3d8
Peter "Marenthyu" Fredebold <petersilie93@live.de>
2021-10-23 19:12:34 +0200

    Code / Deprecation cleanup

--- itemtracker.js
+++ itemtracker.js
@@ -1,11 +1,11 @@
-var PAD = localStorage.getItem("RABI_PAD") ? parseInt(localStorage.getItem("RABI_PAD")) : 5;
-var SIZE_X = localStorage.getItem("RABI_SIZE_X") ? parseInt(localStorage.getItem("RABI_SIZE_X")) : 512;
-var SIZE_Y = localStorage.getItem("RABI_SIZE_Y") ? parseInt(localStorage.getItem("RABI_SIZE_Y")) : 200;
-var RES_X = SIZE_X + PAD*2;
-var RES_Y = SIZE_Y + PAD*2;
-var backgroundColor = localStorage.getItem("RABI_BG_COLOR") ? localStorage.getItem("RABI_BG_COLOR") : "#000000";
+let PAD = localStorage.getItem("RABI_PAD") ? parseInt(localStorage.getItem("RABI_PAD")) : 5;
+let SIZE_X = localStorage.getItem("RABI_SIZE_X") ? parseInt(localStorage.getItem("RABI_SIZE_X")) : 512;
+let SIZE_Y = localStorage.getItem("RABI_SIZE_Y") ? parseInt(localStorage.getItem("RABI_SIZE_Y")) : 200;
+let RES_X = SIZE_X + PAD * 2;
+let RES_Y = SIZE_Y + PAD * 2;
+let backgroundColor = localStorage.getItem("RABI_BG_COLOR") ? localStorage.getItem("RABI_BG_COLOR") : "#000000";
 
-var DEFAULT_ITEM_LIST = "PIKO_HAMMER\n" +
+const DEFAULT_ITEM_LIST = "PIKO_HAMMER\n" +
     "SLIDING_POWDER\n" +
     "RABI_SLIPPERS\n" +
     "AIR_JUMP\n" +
@@ -30,261 +30,61 @@ var DEFAULT_ITEM_LIST = "PIKO_HAMMER\n" +
     "RAINBOW_EGG\n" +
     "RAINBOW_EGG";
 
-var DEFAULT_TOGGLE_ORDER = "OFF\n" +
+const DEFAULT_TOGGLE_ORDER = "OFF\n" +
     "ON\n" +
     "CROSS\n" +
     "CIRCLE";
 
-var currentItemList = localStorage.getItem("RABI_ITEM_LIST") ? JSON.parse(localStorage.getItem("RABI_ITEM_LIST")).join("\n") : DEFAULT_ITEM_LIST;
+const currentItemList = localStorage.getItem("RABI_ITEM_LIST") ? JSON.parse(localStorage.getItem("RABI_ITEM_LIST")).join("\n") : DEFAULT_ITEM_LIST;
 document.getElementById("itemList").innerHTML = currentItemList;
-var currentToggleOrder = localStorage.getItem("RABI_TOGGLE_ORDER") ? JSON.parse(localStorage.getItem("RABI_TOGGLE_ORDER")).join("\n") : DEFAULT_TOGGLE_ORDER;
+const currentToggleOrder = localStorage.getItem("RABI_TOGGLE_ORDER") ? JSON.parse(localStorage.getItem("RABI_TOGGLE_ORDER")).join("\n") : DEFAULT_TOGGLE_ORDER;
 document.getElementById("modeList").innerHTML = currentToggleOrder;
 
 document.getElementById("canvasSizeX").setAttribute("value", SIZE_X);
 document.getElementById("canvasSizeY").setAttribute("value", SIZE_Y);
 document.getElementById("backgroundColor").value = backgroundColor;
 
-var mainCanvas = document.getElementById('mainCanvas');
+const mainCanvas = document.getElementById('mainCanvas');
 mainCanvas.width = RES_X;
 mainCanvas.height = RES_Y;
-var ctx = mainCanvas.getContext('2d');
+const ctx = mainCanvas.getContext('2d');
 
-var win = document.getElementById('window');
-//window.addEventListener("keydown", keyboardPress, false);
-//window.addEventListener("keyup", keyboardRelease, false);
 window.addEventListener("click", mouseClick, false);
 
-var spriteSheetActive = new Image();
+const spriteSheetActive = new Image();
 spriteSheetActive.src = "item_active.png";
+let hasChangeToDraw;
 spriteSheetActive.addEventListener('load', function() {hasChangeToDraw = true;});
-var spriteSheetInactive = new Image();
+const spriteSheetInactive = new Image();
 spriteSheetInactive.src = "item_inactive.png";
 spriteSheetInactive.addEventListener('load', function() {hasChangeToDraw = true;});
-var hasChangeToDraw = false;
+hasChangeToDraw = false;
 
 
-var MODE_OFF = 0;
-var MODE_ON = 1;
-var MODE_CROSS = 2;
-var MODE_CIRCLE = 3;
+const MODE_OFF = 0;
+const MODE_ON = 1;
+const MODE_CROSS = 2;
+const MODE_CIRCLE = 3;
 
-var ICON_WIDTH = 64;
+const ICON_WIDTH = 64;
 
-var itemIcons = [];
-var toggleList = [0,2,3,1];
+let itemIcons = [];
+let toggleList = [0, 2, 3, 1];
 
-var errorList = [];
+let errorList = [];
+
 function error(message) {
     errorList.push(message);
 }
 
-function initializeBoard() {
-    errorList = [];
-    var items = document.getElementById("itemList").value.split("\n");
-    localStorage.setItem("RABI_ITEM_LIST", JSON.stringify(items));
-    var item_id_list = [];
-    for (var i=0; i<items.length; ++i) {
-        var key = items[i].trim().toUpperCase();
-        if (key.length <= 0) {
-            item_id_list.push(null);
-            continue;
-        }
-        if (!ITEM_ID_MAP.hasOwnProperty(key)) {
-            error("Unknown item: " + key);
-        }
-        item_id_list.push(ITEM_ID_MAP[key]);
-    }
-
-    var modes = document.getElementById("modeList").value.split("\n");
-    localStorage.setItem("RABI_TOGGLE_ORDER", JSON.stringify(modes));
-    var mode_id_list = [];
-    for (var i=0; i<modes.length; ++i) {
-        var key = modes[i].trim().toUpperCase();
-        if (key.length <= 0) continue;
-        if (!MODE_ID_MAP.hasOwnProperty(key)) {
-            error("Unknown toggle: " + key);
-        }
-        mode_id_list.push(MODE_ID_MAP[key]);
-    }
-
-    var sizeX = Number(document.getElementById("canvasSizeX").value);
-    var sizeY = Number(document.getElementById("canvasSizeY").value);
-
-    var bgColorText = document.getElementById("backgroundColor").value;
-    if (bgColorText.trim().length === 0) {
-        backgroundColor = null;
-    } else {
-        backgroundColor = bgColorText;
-    }
-
-    localStorage.setItem("RABI_BG_COLOR", backgroundColor);
-
-    var errorMessage = "";
-    if (errorList.length > 0) {
-        errorMessage = "Error:<br>" + errorList.join("<br>")
-        console.log("Error:\n" + errorList.join("\n"));
-    }
-    document.getElementById("errorBox").innerHTML = errorMessage;
-    
-    if (errorList.length === 0) {
-        resizeCanvas(sizeX, sizeY);
-        setupBoard(item_id_list, mode_id_list);
-    }   
-}
-
-function resizeCanvas(sizeX, sizeY) {
-    SIZE_X = sizeX;
-    SIZE_Y = sizeY;
-    RES_X = sizeX + PAD*2;
-    RES_Y = sizeY + PAD*2;
-
-    mainCanvas.width = RES_X;
-    mainCanvas.height = RES_Y;
-
-    localStorage.setItem("RABI_SIZE_X", SIZE_X.toString());
-    localStorage.setItem("RABI_SIZE_Y", SIZE_Y.toString());
-
-}
-
-function addItem(index, item_id) {
-    var cols = Math.floor(SIZE_X/ICON_WIDTH);
-    var px = ICON_WIDTH * (index%cols);
-    var py = ICON_WIDTH * Math.floor(index/cols);
-    itemIcons.push(new ItemIcon(px, py, item_id));
-}
-
-function setupBoard(id_list, toggle_list) {
-    toggleList = toggle_list;
-    itemIcons = [];
-
-    for (var i=0; i<id_list.length; ++i) {
-        if (id_list[i] == null) continue;
-        addItem(i, id_list[i]);
-    }
-
-    hasChangeToDraw = true;
-}
-
-function drawItem(x1, y1, index, current_mode) {
-    x1 += PAD;
-    y1 += PAD;
-
-    var sx = ICON_WIDTH*(index%32);
-    var sy = ICON_WIDTH*Math.floor(index/32);
-    if (current_mode === MODE_ON) {
-        var chosenSpriteSheet = spriteSheetActive;
-    } else {
-        var chosenSpriteSheet = spriteSheetInactive;
-    }
-
-    if (current_mode === MODE_ON) {
-        var radius = ICON_WIDTH/2
-        var thickness = 3
-        ctx.beginPath();
-        ctx.arc(x1+radius, y1+radius, radius-thickness, 0, 2 * Math.PI, false);
-        ctx.fillStyle = '#ffff0040';
-        ctx.fill();
-    }
-
-    ctx.save();
-    ctx.imageSmoothingEnabled = false;
-    ctx.drawImage(chosenSpriteSheet,sx,sy,ICON_WIDTH,ICON_WIDTH,x1,y1,ICON_WIDTH,ICON_WIDTH);
-    ctx.restore();
-
-    if (current_mode === MODE_CROSS) {
-        var thickness = 6;
-        var offset = 6;
-
-        ctx.lineWidth = thickness;
-        ctx.strokeStyle = '#ff0000';
-
-        ctx.beginPath();
-        ctx.moveTo(x1+offset,y1+offset);
-        ctx.lineTo(x1+ICON_WIDTH-offset,y1+ICON_WIDTH-offset);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(x1+offset,y1+ICON_WIDTH-offset);
-        ctx.lineTo(x1+ICON_WIDTH-offset,y1+offset);
-        ctx.stroke();
-    }
-    if (current_mode === MODE_CIRCLE) {
-        var radius = ICON_WIDTH/2
-        var thickness = 6
-        ctx.beginPath();
-        ctx.arc(x1+radius, y1+radius, radius-thickness, 0, 2 * Math.PI, false);
-        ctx.lineWidth = thickness;
-        ctx.strokeStyle = '#ff0000';
-        ctx.stroke();
-    }
-}
-
-var ItemIcon = function(x1, y1, item_id) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.item_id = item_id;
-    this.modeIndex = 0;
-}
-
-ItemIcon.prototype = {
-    withinBounds: function(cx, cy) {
-        return cx >= this.x1 && cy >= this.y1 && cx < this.x1+ICON_WIDTH && cy < this.y1+ICON_WIDTH;
-    },
-
-    toggleMode: function() {
-        this.modeIndex = (this.modeIndex+1)%toggleList.length;
-        hasChangeToDraw = true;
-    },
-
-    draw: function() {
-        this.modeIndex = this.modeIndex%toggleList.length;
-        var mode = toggleList[this.modeIndex];
-        drawItem(this.x1, this.y1, this.item_id, mode);
-    },
-}
-
-function mouseClick(e) {
-    var canvasRect = mainCanvas.getBoundingClientRect();
-    var mouseX = e.clientX - canvasRect.left - PAD;
-    var mouseY = e.clientY - canvasRect.top - PAD;
-    if (mouseX < 0 || mouseY < 0 || mouseX > SIZE_X || mouseY > SIZE_Y) return;
-
-    for (var i=0; i<itemIcons.length; ++i) {
-        if (itemIcons[i].withinBounds(mouseX, mouseY)) {
-            itemIcons[i].toggleMode();
-        }
-    }
-}
-
-function drawFrame() {
-    for (var i=0; i<itemIcons.length; ++i) {
-        itemIcons[i].draw();
-    }
-}
-
-function gameLoop(time){
-    if (hasChangeToDraw) {
-        hasChangeToDraw = false;
-        ctx.clearRect(0, 0, RES_X, RES_Y);
-        if (backgroundColor !== null) {
-            ctx.fillStyle = backgroundColor;
-            ctx.fillRect(0, 0, RES_X, RES_Y);
-        }
-        drawFrame();
-    }
-    window.requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
-
-var MODE_ID_MAP = {
+const MODE_ID_MAP = {
     "OFF": MODE_OFF,
     "ON": MODE_ON,
     "CROSS": MODE_CROSS,
     "CIRCLE": MODE_CIRCLE
 };
 
-var ITEM_ID_MAP = {
+const ITEM_ID_MAP = {
     "NATURE_ORB": 20,
     "BLESSED": 86,
     "TOXIC_STRIKE": 81,
@@ -374,5 +174,212 @@ var ITEM_ID_MAP = {
     "PACK_UP": 100,
     "RAINBOW_EGG": 47
 };
+
+function initializeBoard() {
+    let key;
+    let i;
+    errorList = [];
+    const items = document.getElementById("itemList").value.split("\n");
+    localStorage.setItem("RABI_ITEM_LIST", JSON.stringify(items));
+    const item_id_list = [];
+    for (i = 0; i<items.length; ++i) {
+        key = items[i].trim().toUpperCase();
+        if (key.length <= 0) {
+            item_id_list.push(null);
+            continue;
+        }
+        if (!ITEM_ID_MAP.hasOwnProperty(key)) {
+            error("Unknown item: " + key);
+        }
+        item_id_list.push(ITEM_ID_MAP[key]);
+    }
+
+    const modes = document.getElementById("modeList").value.split("\n");
+    localStorage.setItem("RABI_TOGGLE_ORDER", JSON.stringify(modes));
+    const mode_id_list = [];
+    for (i = 0; i<modes.length; ++i) {
+        key = modes[i].trim().toUpperCase();
+        if (key.length <= 0) continue;
+        if (!MODE_ID_MAP.hasOwnProperty(key)) {
+            error("Unknown toggle: " + key);
+        }
+        mode_id_list.push(MODE_ID_MAP[key]);
+    }
+
+    const sizeX = Number(document.getElementById("canvasSizeX").value);
+    const sizeY = Number(document.getElementById("canvasSizeY").value);
+
+    const bgColorText = document.getElementById("backgroundColor").value;
+    if (bgColorText.trim().length === 0) {
+        backgroundColor = null;
+    } else {
+        backgroundColor = bgColorText;
+    }
+
+    localStorage.setItem("RABI_BG_COLOR", backgroundColor);
+
+    let errorMessage = "";
+    if (errorList.length > 0) {
+        errorMessage = "Error:<br>" + errorList.join("<br>")
+        console.log("Error:\n" + errorList.join("\n"));
+    }
+    document.getElementById("errorBox").innerHTML = errorMessage;
+    
+    if (errorList.length === 0) {
+        resizeCanvas(sizeX, sizeY);
+        setupBoard(item_id_list, mode_id_list);
+    }   
+}
+
+function resizeCanvas(sizeX, sizeY) {
+    SIZE_X = sizeX;
+    SIZE_Y = sizeY;
+    RES_X = sizeX + PAD*2;
+    RES_Y = sizeY + PAD*2;
+
+    mainCanvas.width = RES_X;
+    mainCanvas.height = RES_Y;
+
+    localStorage.setItem("RABI_SIZE_X", SIZE_X.toString());
+    localStorage.setItem("RABI_SIZE_Y", SIZE_Y.toString());
+
+}
+
+const ItemIcon = function(x1, y1, item_id) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.item_id = item_id;
+    this.modeIndex = 0;
+};
+
+function addItem(index, item_id) {
+    const cols = Math.floor(SIZE_X / ICON_WIDTH);
+    const px = ICON_WIDTH * (index % cols);
+    const py = ICON_WIDTH * Math.floor(index / cols);
+    itemIcons.push(new ItemIcon(px, py, item_id));
+}
+
+function setupBoard(id_list, toggle_list) {
+    toggleList = toggle_list;
+    itemIcons = [];
+
+    for (let i=0; i<id_list.length; ++i) {
+        if (id_list[i] == null) continue;
+        addItem(i, id_list[i]);
+    }
+
+    hasChangeToDraw = true;
+}
+
+function drawItem(x1, y1, index, current_mode) {
+    let thickness;
+    let radius;
+    let chosenSpriteSheet;
+    x1 += PAD;
+    y1 += PAD;
+
+    const sx = ICON_WIDTH * (index % 32);
+    const sy = ICON_WIDTH * Math.floor(index / 32);
+    if (current_mode === MODE_ON) {
+        chosenSpriteSheet = spriteSheetActive;
+    } else {
+        chosenSpriteSheet = spriteSheetInactive;
+    }
+
+    if (current_mode === MODE_ON) {
+        radius = ICON_WIDTH/2;
+        thickness = 3;
+        ctx.beginPath();
+        ctx.arc(x1+radius, y1+radius, radius-thickness, 0, 2 * Math.PI, false);
+        ctx.fillStyle = '#ffff0040';
+        ctx.fill();
+    }
+
+    ctx.save();
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(chosenSpriteSheet,sx,sy,ICON_WIDTH,ICON_WIDTH,x1,y1,ICON_WIDTH,ICON_WIDTH);
+    ctx.restore();
+
+    if (current_mode === MODE_CROSS) {
+        thickness = 6;
+        const offset = 6;
+
+        ctx.lineWidth = thickness;
+        ctx.strokeStyle = '#ff0000';
+
+        ctx.beginPath();
+        ctx.moveTo(x1+offset,y1+offset);
+        ctx.lineTo(x1+ICON_WIDTH-offset,y1+ICON_WIDTH-offset);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(x1+offset,y1+ICON_WIDTH-offset);
+        ctx.lineTo(x1+ICON_WIDTH-offset,y1+offset);
+        ctx.stroke();
+    }
+    if (current_mode === MODE_CIRCLE) {
+        radius = ICON_WIDTH / 2;
+        thickness = 6;
+        ctx.beginPath();
+        ctx.arc(x1+radius, y1+radius, radius-thickness, 0, 2 * Math.PI, false);
+        ctx.lineWidth = thickness;
+        ctx.strokeStyle = '#ff0000';
+        ctx.stroke();
+    }
+}
+
+
+ItemIcon.prototype = {
+    withinBounds: function(cx, cy) {
+        return cx >= this.x1 && cy >= this.y1 && cx < this.x1+ICON_WIDTH && cy < this.y1+ICON_WIDTH;
+    },
+
+    toggleMode: function() {
+        this.modeIndex = (this.modeIndex+1)%toggleList.length;
+        hasChangeToDraw = true;
+    },
+
+    draw: function() {
+        this.modeIndex = this.modeIndex%toggleList.length;
+        const mode = toggleList[this.modeIndex];
+        drawItem(this.x1, this.y1, this.item_id, mode);
+    },
+}
+
+function mouseClick(e) {
+    const canvasRect = mainCanvas.getBoundingClientRect();
+    const mouseX = e.clientX - canvasRect.left - PAD;
+    const mouseY = e.clientY - canvasRect.top - PAD;
+    if (mouseX < 0 || mouseY < 0 || mouseX > SIZE_X || mouseY > SIZE_Y) return;
+
+    for (let i=0; i<itemIcons.length; ++i) {
+        if (itemIcons[i].withinBounds(mouseX, mouseY)) {
+            itemIcons[i].toggleMode();
+        }
+    }
+}
+
+function drawFrame() {
+    for (let i=0; i<itemIcons.length; ++i) {
+        itemIcons[i].draw();
+    }
+}
+
+function gameLoop(){
+    if (hasChangeToDraw) {
+        hasChangeToDraw = false;
+        ctx.clearRect(0, 0, RES_X, RES_Y);
+        if (backgroundColor !== null) {
+            ctx.fillStyle = backgroundColor;
+            ctx.fillRect(0, 0, RES_X, RES_Y);
+        }
+        drawFrame();
+    }
+    window.requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
+
 
 
